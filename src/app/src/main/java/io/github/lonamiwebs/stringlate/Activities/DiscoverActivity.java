feacb72b6f8f81d242fe1952ee880010d3caefc8
Lonami Exo <totufals@hotmail.com>
2016-12-06 18:34:34 +0100

    Added a list view to the discover activity, new app-not-found icon

--- src/app/src/main/java/io/github/lonamiwebs/stringlate/Activities/DiscoverActivity.java
+++ src/app/src/main/java/io/github/lonamiwebs/stringlate/Activities/DiscoverActivity.java
@@ -2,7 +2,11 @@ package io.github.lonamiwebs.stringlate.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import io.github.lonamiwebs.stringlate.R;
 
@@ -12,9 +16,33 @@ public class DiscoverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                data,
+                R.layout.item_application_list,
+                new String[] { "Icon", "Name", "Description" },
+                new int[] { R.id.appIcon, R.id.appName, R.id.appDescription });
+
+        ((ListView)findViewById(R.id.packageListView)).setAdapter(adapter);
     }
 
-    public void onExitClick(View v) {
-        finish();
+    final static ArrayList<HashMap<String, ?>> data = new ArrayList<HashMap<String, ?>>();
+
+    static {
+        HashMap<String, Object> row  = new HashMap<String, Object>();
+        row.put("Icon", R.drawable.app_not_found);
+        row.put("Name", "Stringlate");
+        row.put("Description", "Official version");
+        data.add(row);
+        row  = new HashMap<String, Object>();
+        row.put("Icon", R.drawable.app_not_found);
+        row.put("Name", "Stringlate");
+        row.put("Description", "Absolutely not hacked version");
+        data.add(row);
+        row  = new HashMap<String, Object>();
+        row.put("Icon", R.drawable.app_not_found);
+        row.put("Name", "Cadenaductor");
+        row.put("Description", "Stringlate versión española");
+        data.add(row);
     }
 }
