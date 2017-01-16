dc09b25a0b307bab40e57a0f8a25e7e04388e426
Lonami Exo <totufals@hotmail.com>
2017-01-16 20:55:14 +0100

    Improve the accuracy when showing "cloning repository"

--- src/app/src/main/java/io/github/lonamiwebs/stringlate/git/GitCloneProgressCallback.java
+++ src/app/src/main/java/io/github/lonamiwebs/stringlate/git/GitCloneProgressCallback.java
@@ -11,66 +11,91 @@ public abstract class GitCloneProgressCallback
         implements ProgressUpdateCallback, ProgressMonitor {
 
     private Activity mActivity;
-    private int mCurrentTask, mDone;
-    private float mWork;
+    private int mCurrentTask, mDone, mWork;
+
+    private long mLastMs;
+
+    private final static long DELAY_PER_UPDATE = 60; // 60ms
 
     protected GitCloneProgressCallback(Activity activity) {
         mActivity = activity;
     }
 
     @Override
-    public void start(int totalTasks) {
-        mCurrentTask = 0;
-    }
-
-    @Override
-    public void beginTask(String title, int totalWork) {
+    final public void beginTask(String title, int totalWork) {
         mCurrentTask++;
+        mLastMs = 0;
         mDone = 0;
         mWork = totalWork;
     }
 
     @Override
-    public void update(int completed) {
+    final public void update(int completed) {
         mDone += completed;
-        updateProgress();
-    }
 
-    @Override
-    public void endTask() {
-        mDone = (int)mWork;
-        updateProgress();
-    }
-
-    private void updateProgress() {
-        if (mWork > 0) {
-            // We cannot know the task count before hand, so we only have
-            // mCurrentTask and we assume that these are the total tasks.
-            // The current task averages the most-1 or it would be 100%.
-            //
-            // Then, we simply multiply the current work done by how much
-            // we would fill if that task were done. Say we have 3/4 tasks
-            // done, then the work should fill that 25%.
-            float perTask = 1.0f / mCurrentTask;
-            float progress = 100.0f *
-                    (((mCurrentTask - 1.0f) * perTask) +
-                            ((mDone / mWork) * perTask));
-
-            final String title = mActivity.getString(R.string.cloning_repo);
-            final String content = mActivity.getString(
-                    R.string.cloning_repo_progress, progress);
-
-            mActivity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    onProgressUpdate(title, content);
-                }
-            });
+        // This method is called way so often, slow it down
+        long time = System.currentTimeMillis();
+        if (time - mLastMs >= DELAY_PER_UPDATE) {
+            mLastMs = time;
+            updateProgress();
         }
     }
 
     @Override
-    public boolean isCancelled() {
+    final public void start(int totalTasks) { /* Total tasks is a liar */ }
+
+    @Override
+    final public void endTask() { }
+
+    private void updateProgress() {
+        final String title = mActivity.getString(R.string.cloning_repo);
+        final String content = mActivity.getString(
+                R.string.cloning_repo_progress, getProgress(mCurrentTask, mDone, mWork));
+
+        // TODO This probably can be improved. Some handler to post the result?
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                onProgressUpdate(title, content);
+            }
+        });
+    }
+
+    private static float getProgress(int task, float done, float work) {
+        // After timing cloning a large repository (F-Droid client) twice and averaging:
+        // Task 1 → 2: 2ms     (0.003%)
+        // Task 2 → 3: 125ms   (0.204%)
+        // Task 3 → 4: 28010ms (45.74%)
+        // Task 4 → 5: 33100ms (54.05%)
+        if (work > 0 && task < 5) {
+            return getTotalTime(task) + (done / work) * getTime(task);
+        } else {
+            return getTotalTime(task);
+        }
+    }
+
+    private static float getTime(int task) {
+        switch (task) {
+            case 1: return 0.003f;
+            case 2: return 0.204f;
+            case 3: return 45.74f;
+            case 4: return 54.52f;
+            default: return 0.00f;
+        }
+    }
+
+    private static float getTotalTime(int untilTask) {
+        switch (untilTask) {
+            case 1: return 0.000f;
+            case 2: return 0.003f;
+            case 3: return 0.207f;
+            case 4: return 45.95f;
+            default: return 1.00f;
+        }
+    }
+
+    @Override
+    final public boolean isCancelled() {
         return false;
     }
 }
