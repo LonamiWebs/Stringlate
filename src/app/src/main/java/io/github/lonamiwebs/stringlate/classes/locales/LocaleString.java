e28fc6f8fea2f6e2b2f1e41f228fc2d3b35c47d4
Lonami Exo <totufals@hotmail.com>
2017-09-07 14:11:07 +0200

    Fix and enhance get display methods on LocaleString

--- src/app/src/main/java/io/github/lonamiwebs/stringlate/classes/locales/LocaleString.java
+++ src/app/src/main/java/io/github/lonamiwebs/stringlate/classes/locales/LocaleString.java
@@ -31,52 +31,38 @@ public class LocaleString {
         return mLocaleDisplay;
     }
 
-    public static String getDisplay(@NonNull String locale) {
-        // If there is an hyphen, then a country was also specified
-        // Not sure why but the 'r' prefix is needed for the country
-        if (locale.contains("-")) {
-            for (Locale l : Locale.getAvailableLocales())
-                if (!l.getCountry().isEmpty())
-                    if (locale.equals(l.getLanguage() + "-r" + l.getCountry()))
-                        return l.getDisplayName();
-        } else {
-            for (Locale l : Locale.getAvailableLocales())
-                if (locale.equals(l.getLanguage()))
-                    if (l.getCountry().isEmpty()) // Ensure that it's empty (issue #58)
-                        return l.getDisplayName();
-        }
-        return locale;
+    public static String getDisplay(@NonNull String localeCode) {
+        final Locale locale = getLocale(localeCode);
+        if (isValid(locale))
+            return locale.getDisplayName();
+        else
+            return localeCode;
     }
 
     // Useful when exporting to issue for example
-    public static String getEnglishDisplay(@NonNull String locale) {
-        if (locale.contains("-")) {
-            for (Locale l : Locale.getAvailableLocales())
-                if (!l.getCountry().isEmpty())
-                    if (locale.equals(l.getLanguage() + "-r" + l.getCountry()))
-                        return l.getDisplayName(Locale.ENGLISH);
-        } else {
-            for (Locale l : Locale.getAvailableLocales())
-                if (locale.equals(l.getLanguage()))
-                    if (l.getCountry().isEmpty())
-                        return l.getDisplayName(Locale.ENGLISH);
-        }
-        return locale;
+    public static String getEnglishDisplay(@NonNull String localeCode) {
+        final Locale locale = getLocale(localeCode);
+        if (isValid(locale))
+            return locale.getDisplayName(Locale.ENGLISH);
+        else
+            return localeCode;
     }
 
+    @NonNull
     public static String getFullCode(@NonNull Locale locale) {
         String language = locale.getLanguage();
         String country = locale.getCountry();
         return country.isEmpty() ? language : language + "-r" + country;
     }
 
-    public static boolean isValid(@NonNull String locale) {
+    @NonNull
+    public static Locale getLocale(@NonNull String locale) {
         if (locale.contains("-")) {
             final String[] parts = locale.split("-");
-            return isValid(new Locale(parts[0], parts[1].substring(1)));
+            return new Locale(parts[0], parts[1].substring(1));
         }
 
-        return isValid(new Locale(locale));
+        return new Locale(locale);
     }
 
     private static boolean isValid(final Locale locale) {
@@ -85,21 +71,6 @@ public class LocaleString {
         } catch (MissingResourceException ignored) {
             return false;
         }
-    }
-
-    @NonNull
-    public static String sanitizeLocale(String locale) {
-        locale = locale.trim();
-        Matcher m = SANITIZE_PATTERN.matcher(locale);
-        if (m.find()) {
-            String lang = m.group(1).toLowerCase();
-            String country = m.group(2);
-            if (country != null)
-                return String.format("%s-r%s", lang, country.toUpperCase());
-            else
-                return lang;
-        }
-        return locale;
     }
 
     @NonNull
