faf967ca87711f296f8407044fef3fea71fe6982
Lonami Exo <totufals@hotmail.com>
2018-02-24 11:39:21 +0100

    Save string filter only once
    After the filter is passed to the translate activity, it then gets
    saved on the settings, so doing so from the search activity is only
    redundant.

--- src/app/src/main/java/io/github/lonamiwebs/stringlate/activities/translate/SearchStringActivity.java
+++ src/app/src/main/java/io/github/lonamiwebs/stringlate/activities/translate/SearchStringActivity.java
@@ -86,13 +86,7 @@ public class SearchStringActivity extends AppCompatActivity {
         mResourcesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // Save the current search, although we also need to pass it
-                // with the intent because the json is not reloaded from the
-                // file when we're asked for the updated string (on Translate)
                 final String filter = mSearchEditText.getText().toString();
-                mRepo.settings.setStringFilter(filter);
-
-                // Return the selected string to the parent activity
                 ResourcesTranslation rt = (ResourcesTranslation) mResourcesListView.getItemAtPosition(i);
                 Intent data = new Intent();
                 data.putExtra("id", rt.getId());
