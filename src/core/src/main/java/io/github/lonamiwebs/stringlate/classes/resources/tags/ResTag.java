910ddf001371f74d743b430d3851c3ed4e1aae32
Lonami Exo <totufals@hotmail.com>
2018-03-07 16:46:16 +0100

    Revisit desanitizeContent

--- src/core/src/main/java/io/github/lonamiwebs/stringlate/classes/resources/tags/ResTag.java
+++ src/core/src/main/java/io/github/lonamiwebs/stringlate/classes/resources/tags/ResTag.java
@@ -85,85 +85,7 @@ public abstract class ResTag implements Comparable<ResTag> {
 
     //endregion
 
-    //region
-
-    // De-sanitizes the content, making it ready to modified outside a strings.xml file
-    public static String desanitizeContent(String content) {
-        char c;
-        int length = content.length();
-        StringBuilder sb = new StringBuilder(length + 16); // 16 seems to be the default capacity
-
-        // Currently we handle the following sequences:
-        // \", \', \\, \n, &lt;, &gt;, &amp;
-        for (int i = 0; i < length; i++) {
-            c = content.charAt(i);
-            switch (c) {
-                // Unescape these sequences iff we know them
-                case '\\':
-                    i++; // Handle the next character here
-                    if (i >= length) {
-                        sb.append('\\');
-                        break;
-                    }
-                    c = content.charAt(i);
-                    switch (c) {
-                        case '"':
-                        case '\'':
-                        case '\\':
-                            sb.append(c);
-                            break;
-                        case 'n':
-                            sb.append('\n');
-                            break;
-                        default:
-                            // Keep the unescaped sequence
-                            // TODO We'll have problems with this because then the \s will be \\s
-                            // since we will escape the \ as \\
-                            sb.append('\\').append(c);
-                            break;
-                    }
-                    break;
-
-                case '&':
-                    int semicolon = content.indexOf(';', i);
-                    if (semicolon < 0) {
-                        // Invalid string actually
-                        // TODO Handle invalid & on the middle, but escaped OK later?
-                        // (i.e., check whitespace after &)
-                        sb.append('&');
-                    } else {
-                        switch (content.substring(i, semicolon)) {
-                            case "&lt":
-                                sb.append('<');
-                                break;
-                            case "&gt":
-                                sb.append('>');
-                                break;
-                            case "&amp":
-                                sb.append('&');
-                                break;
-                            default:
-                                // Keep the unescaped sequence, we'll also have trouble
-                                sb.append(content.substring(i, semicolon)).append(';');
-                                break;
-                        }
-                        i = semicolon;
-                    }
-                    break;
-
-                // New lines are treated as spaces
-                case '\n':
-                    sb.append(' ');
-                    break;
-
-                // Normal character
-                default:
-                    sb.append(c);
-                    break;
-            }
-        }
-        return sb.toString();
-    }
+    //region De/sanitize Content
 
     private static boolean isEscapeSequence(char which) {
         for (int i = 0; i < ESCAPE_SEQUENCES.length; ++i) {
@@ -196,6 +118,78 @@ public abstract class ResTag implements Comparable<ResTag> {
         return false;
     }
 
+    // De-sanitizes the content, making it ready to modified outside a strings.xml file
+    public static String desanitizeContent(String content) {
+        char c;
+        int length = content.length();
+        StringBuilder sb = new StringBuilder(length + 16); // 16 seems to be the default capacity
+
+        for (int i = 0; i < length; i++) {
+            c = content.charAt(i);
+            switch (c) {
+                // Unescape these sequences iff we know them
+                case '\\':
+                    i++; // Handle the next character here
+                    if (i >= length) {
+                        sb.append('\\');
+                        break;
+                    }
+                    c = content.charAt(i);
+                    switch (c) {
+                        case '"':
+                        case '\'':
+                        case '\\':
+                            sb.append(c);
+                            break;
+                        case 'n':
+                            sb.append('\n');
+                            break;
+                        default:
+                            sb.append('\\').append(c);
+                            break;
+                    }
+                    break;
+
+                case '&':
+                    int semicolon = content.indexOf(';', i);
+                    if (semicolon < 0) {
+                        // Invalid string actually, but nothing we can do.
+                        sb.append('&');
+                    } else {
+                        switch (content.substring(i, semicolon)) {
+                            // These are some of the most-common and "dangerous" cases,
+                            // since not handling them in "sanitize" would cause invalid XML.
+                            case "&lt":
+                                sb.append('<');
+                                break;
+                            case "&gt":
+                                sb.append('>');
+                                break;
+                            case "&amp":
+                                sb.append('&');
+                                break;
+                            default:
+                                sb.append(content.substring(i, semicolon)).append(';');
+                                break;
+                        }
+                        i = semicolon;
+                    }
+                    break;
+
+                // New lines are treated as spaces
+                case '\n':
+                    sb.append(' ');
+                    break;
+
+                // Normal character
+                default:
+                    sb.append(c);
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+
     // Sanitizes the content, making it ready to be written to a strings.xml file
     public static String sanitizeContent(String content) {
         char c;
@@ -221,9 +215,6 @@ public abstract class ResTag implements Comparable<ResTag> {
             }
         }
 
-        // Currently we handle the following characters:
-        // \n, ', \, &, <, >, "
-        //
         // Keep track of insideAngleBrackets to escape " only if we're outside.
         boolean insideAngleBrackets = false;
         for (int i = 0; i < length; i++) {
